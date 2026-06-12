2509b05f97f6b92984d43bee07047b0cfc4c82c2
SUNIL KUMAR <kumarsunil100403@gmail.com>
2026-06-12 19:30:36 +0530

    Solve rmsprop-optimizer

---
+++ rmsprop-optimizer/rmsprop-optimizer.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+def rmsprop_step(w, g, s, lr=0.001, beta=0.9, eps=1e-8):
+    """
+    Perform one RMSProp update step.
+    """
+
+    w = np.asarray(w, dtype=float)
+    g = np.asarray(g, dtype=float)
+    s = np.asarray(s, dtype=float)
+
+    # Update running squared gradient average
+    s_new = beta * s + (1 - beta) * (g ** 2)
+
+    # Update parameters
+    w_new = w - lr * g / (np.sqrt(s_new) + eps)
+
+    return w_new, s_new
+    pass 
