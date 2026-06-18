c6ae08af95d0bcd211201f3dce794febd15b801e
SUNIL KUMAR <kumarsunil100403@gmail.com>
2026-06-18 06:07:38 +0530

    Solve matrix-normalization

--- matrix-normalization/matrix-normalization.py
+++ matrix-normalization/matrix-normalization.py
@@ -4,27 +4,30 @@ def matrix_normalization(matrix, axis=None, norm_type='l2'):
     """
     Normalize a 2D matrix along specified axis using specified norm.
     """
- 
-    X = np.asarray(matrix)
-    if X.ndim != 2:
-        return None
 
-    if axis not in (None, 0, 1):
-        return None
-        
-    if norm_type == 'l1':
-        norm = np.sum(abs(X), axis = axis, keepdims = True)
-        
-    elif norm_type == 'l2':
-        norm = np.sqrt(np.sum(X ** 2, axis = axis, keepdims = True))
-        
-    elif norm_type == 'max':
-        norm = np.max(X, axis = axis, keepdims = True)
-    else:
-        return None
-    
-    norm[norm == 0] = 1
+    try:
+        matrix = np.asarray(matrix, dtype=float)
 
-    return X/ norm
-    
+        if matrix.ndim != 2:
+            return None
+
+        if norm_type == 'l2':
+            norms = np.sqrt(np.sum(matrix**2, axis=axis, keepdims=True))
+
+        elif norm_type == 'l1':
+            norms = np.sum(np.abs(matrix), axis=axis, keepdims=True)
+
+        elif norm_type == 'max':
+            norms = np.max(np.abs(matrix), axis=axis, keepdims=True)
+
+        else:
+            return None
+
+        # Avoid division by zero
+        norms[norms == 0] = 1
+
+        return matrix / norms
+
+    except:
+        return None
     pass
